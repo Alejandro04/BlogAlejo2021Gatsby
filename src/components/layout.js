575bf01c6607aa4ca65bc933d7d94f87ca0fdbcf
Alejandro Roa <alejandro.roa@pelikane.com>
2021-06-17 10:06:58 -0400

    change profile picture

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,7 +17,7 @@ const Layout = ({ location, title, children }) => {
           className="bio-avatar"
           layout="fixed"
           formats={["AUTO", "WEBP", "AVIF"]}
-          src="../images/profile-pic.png"
+          src="../images/profile-pic.jpg"
           width={100}
           height={100}
           quality={95}
